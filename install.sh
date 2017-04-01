14be262d1827a708ad38b181ea9dbe45702902c7
Laurence Bortfeld <lb@nowdiscover.com>
2017-04-01 11:02:54 +0200

    Add tmux configuration

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 D=`cd $(dirname $0) && pwd`;
+REGEX='''^\.[^g]\w+';
 
 if [[ "$1" = "--uninstall" ]]; then
-  ls -a $D | egrep '^\.[^git]\w+' | xargs -I {} sh -c '[[ -L ~/{} ]] && rm ~/{}';
+  ls -a $D | egrep $REGEX | xargs -I {} sh -c '[[ -L ~/{} ]] && rm ~/{} && echo unlink ~/{}';
 
   exit 0;
 fi;
 
-ls -a $D | egrep '^\.[^git]\w+' | xargs -I {} sh -c "[[ -L ~/{} ]] || ln -s $D/{} ~/{}";
+ls -a $D | egrep $REGEX | xargs -I {} sh -c "[[ -L ~/{} ]] || ln -s $D/{} ~/{} && echo link ~/{}";
