3c7ebb57b6bf198a27b4da50ae507d20ac89408c
Laurence Bortfeld <laurence.bortfeld@pidoco.com>
2018-05-09 15:28:56 +0200

    Install fasd

--- install.sh
+++ install.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 
-D=`cd $(dirname $0) && pwd`;
-REGEX='^\.[^g]\w+';
+function exist() {
+  which $1 > /dev/null 2>&1
+}
 
+function install() {
+  exist $1 || {
+    (exist yay && yay $1) || \
+    (exist brew && brew $1)
+  }
+}
+
+D=`cd $(dirname $0) && pwd`
+REGEX='^\.[^g]\w+'
+
+# Remove all link .dot files;
 if [[ "$1" = "--uninstall" ]]; then
-  ls -a $D | egrep $REGEX | xargs -I {} sh -c '[[ -L ~/{} ]] && rm ~/{} && echo unlink ~/{}';
+  ls -a $D | egrep $REGEX | xargs -I {} sh -c '[[ -L ~/{} ]] && rm ~/{} && echo unlink ~/{}'
 
-  exit 0;
-fi;
+  exit 0
+fi
 
-ls -a $D | egrep $REGEX | xargs -I {} sh -c "[[ -L ~/{} ]] || ln -s $D/{} ~/{} && echo link ~/{}";
+# Install all .dot files from this directory
+ls -a $D | egrep $REGEX | xargs -I {} sh -c "[[ -L ~/{} ]] || ln -s $D/{} ~/{} && echo link ~/{}"
+
+
+# Install other stuff;
+install fasd
+
